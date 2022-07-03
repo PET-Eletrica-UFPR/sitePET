d21e0b49dba4367fc5f5a22a40511cdd56c76b4b
Jose Eduardo <joseduardosilva.42@gmail.com>
2022-07-03 19:58:51 -0300

    Ajustada a estilização dos componentes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,37 @@
-import { Flex, Heading, Text } from '@chakra-ui/react';
-
+import { Flex, Heading, Text, Image } from '@chakra-ui/react';
 
 export function Footer() {
   return (
-    <Flex as='footer' flexDirection='column' px={10} py={5}>
-      <Heading size='md'>Contato</Heading>
-      <Text fontSize='sm' mt={5}>Universidade Federal do Paraná</Text>
-      <Text fontSize='sm'>Campus Politécnico</Text>
-      <Text fontSize='sm'>Av. Cel. Francisco H. dos Santos - Jardim das Américas</Text>
-      <Text fontSize='sm' mb={5}>Curitiba - PR. 81530-000</Text>
-      <Text fontSize='sm'>pet@eletrica.ufpr.br</Text>
+    <Flex
+      as='footer' 
+      align='center'
+      bg='#C4C4C4'
+      px={20} 
+      py={10}
+    >
+      <Flex flexDirection='column'  >
+        <Heading size='md'>Contato</Heading>
+        <Text fontSize='sm' mt={5}>Universidade Federal do Paraná</Text>
+        <Text fontSize='sm'>Campus Politécnico</Text>
+        <Text fontSize='sm'>Av. Cel. Francisco H. dos Santos - Jardim das Américas</Text>
+        <Text fontSize='sm' mb={5}>Curitiba - PR. 81530-000</Text>
+        <Text fontSize='sm'>pet@eletrica.ufpr.br</Text>        
+      </Flex>
+      <Flex
+      ml = 'auto'
+      mr = {20}
+      >
+        <Image 
+          src="./public/logoFace.png" 
+          alt="logoFace"
+          boxSize='40px' 
+        />
+        <Image 
+          src="./public/logoInsta.png" 
+          alt="logoInsta" 
+          boxSize='40px'
+        />
+        </Flex>
     </Flex>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,42 @@
-import { Flex, Link } from '@chakra-ui/react';
+import { Flex, Link, Image, HStack } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 
 export function Header() {
   return (
-    <Flex as='header' direction='row' justify='space-between' px={20} py={10}>
-      <Link as={ReactRouterLink} to='/'>Início</Link>
-      <Link as={ReactRouterLink} to='/projects'>Projetos</Link>
-      <Link as={ReactRouterLink} to='/graduation'>Graduação</Link>
-      <Link as={ReactRouterLink} to='/extension'>Extensão</Link>
-      <Link as={ReactRouterLink} to='/events'>Eventos</Link>
-      <Link as={ReactRouterLink} to='/certificates'>Documentos</Link>
-      <Link as={ReactRouterLink} to='/about-us'>Sobre Nós</Link>
+    <Flex 
+    as='header' 
+    direction='row'
+    justify='space-between'
+    align='center'
+    px={20} 
+    bg='#0045AD'
+    color='#A8A8B3'
+    >
+      <Flex
+      >
+        <Image 
+          src="./public/Logo.svg"
+          alt="logoFace"
+          boxSize='120px' 
+        />
+
+      </Flex>
+      <HStack
+        spacing={20}
+        ml = 'auto'
+        mr='auto'
+      >
+
+        <Link as={ReactRouterLink} to='/'>Início</Link>
+        <Link as={ReactRouterLink} to='/projects'>Projetos</Link>
+        <Link as={ReactRouterLink} to='/graduation'>Graduação</Link>
+        <Link as={ReactRouterLink} to='/extension'>Extensão</Link>
+        <Link as={ReactRouterLink} to='/events'>Eventos</Link>
+        <Link as={ReactRouterLink} to='/certificates'>Documentos</Link>
+        <Link as={ReactRouterLink} to='/about-us'>Sobre Nós</Link>
+
+      </HStack>
     </Flex>
   );
 }
