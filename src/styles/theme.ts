75264366129f5952d0082e88c3ecb655739c3a48
Jose Eduardo <joseduardosilva.42@gmail.com>
2022-07-03 20:00:39 -0300

    Adicionados temas globais, porém não funcionam

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    colors: {     
+        blue: {
+          'pet': '#0045AD'
+        }
+    },
+    
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto'
+    },
+
+    textStyles: {
+        h1: {
+        fontSize: ['36px', '48px'],
+        fontWeight: 'bold'
+        }
+    },
+    
+    styles: {
+        global: {
+            body: {
+                bg: '#E5E5E5',
+                color: '#A8A8B3'
+            }
+        }
+    }
+})
