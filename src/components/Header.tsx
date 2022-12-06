b35ac4e659449fbd9bd9fee383373c71663a4a24
José Eduardo <joseduardosilva.42@gmail.com>
2022-12-06 15:41:58 -0300

    fix colours at active link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,13 +78,13 @@ export function Header() {
         mr='auto'
       >
 
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to="/">Inicio </NavLink>
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to='/projects'>Projetos</NavLink>
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to='/graduation' >Graduação</NavLink>
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to='/extension' >Extensão</NavLink>
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to='/events' >Eventos</NavLink>
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to='/certificates' >Documentos</NavLink>
-        <NavLink style={({ isActive }) => ({ color: isActive ? '#fff' : '#E1E1E6', background: isActive ? '#0045AD' : '#0045AD', })} to='/about-us' >Sobre Nós</NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to="/">Inicio </NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to='/projects'>Projetos</NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to='/graduation' >Graduação</NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to='/extension' >Extensão</NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to='/events' >Eventos</NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to='/certificates' >Documentos</NavLink>
+        <NavLink style={({ isActive }) => ({ color: isActive ? '#E1E1E6' : '#A8A8B3', fontWeight: isActive ? 'Bold' : 'Normal', })} to='/about-us' >Sobre Nós</NavLink>
       </HStack>
     </Flex>
   );
